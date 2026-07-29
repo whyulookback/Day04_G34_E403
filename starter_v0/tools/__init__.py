5e249a7614cdcd7ca27a9bca90f90b451ed33653
anhnvVNU <vanh6bvk@gmail.com>
2026-07-29 12:26:32 +0700

    update lab04

--- starter_v0/tools/__init__.py
+++ starter_v0/tools/__init__.py
@@ -13,6 +13,7 @@ from .paper_text.tool import get_arxiv_paper_text
 from .timeline.tool import get_user_tweets
 from .fetch.tool import read_url
 from .format.tool import render_digest
+from .keywords.tool import extract_keywords
 from .policy.tool import search_company_policy
 from .social_search.tool import search_tweets
 from .send.tool import send_telegram
@@ -31,6 +32,7 @@ TOOL_FUNCTIONS = {
     "lookup": web_search,
     "fetch": read_url,
     "format": render_digest,
+    "keywords": extract_keywords,
     "send": send_telegram,
     "policy": search_company_policy,
     "papers": arxiv_search,
